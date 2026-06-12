07d154dcedac164bd5c670e3a6454b3e01a0fb1e
Naitik1502 <54326426+Naitik1502@users.noreply.github.com>
2026-06-12 04:16:41 -0400

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,53 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+        x_sample = []
+        
+
+        n_rows = len(board)
+        n_cols = len(board[0])
+
+        for i in range(n_rows):
+            row = []
+            for j in range(n_cols):
+                row.append(board[i][j])
+                
+                board[i][j] = "X"
+            x_sample.append(row)
+        
+        print(x_sample)
+
+        visited = set()
+        
+        def dfs(row, col):
+            if not ((-1 < row < n_rows) and (-1< col < n_cols)):
+                return 
+            if x_sample[row][col] == 'X' :
+                return
+            if (row , col) in visited :
+                return
+
+            if x_sample[row][col] == 'O':
+                board[row][col] = 'O'
+                visited.add((row,col))
+
+            for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+                dfs(row +dr, col +dc)
+        
+        for i in range(n_cols):
+            if x_sample[0][i] == "O" :
+                dfs(0,i)
+            if x_sample[n_rows-1][i] == "O" :
+                print(i)
+                dfs(n_rows-1,i)
+        
+        for i in range(n_rows):
+            if x_sample[i][0] == "O" :
+                dfs(i,0)
+            if x_sample[i][n_cols-1] == "O" :
+                dfs(i,n_cols-1)
+        
+
+        
+            
+            
